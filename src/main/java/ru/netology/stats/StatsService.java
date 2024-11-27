bb439eebecc2b1dddd5180e1824d1c68af30c611
KozlovskyS <skozmail@gmail.com>
2024-11-27 18:54:28 +0300

    Corrected names method and type variables

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -2,23 +2,23 @@ package ru.netology.stats;
 
 public class StatsService {
 
-    public int TotalSales(int[] sales) {
-        int totSales = 0;
+    public long totalSales(long[] sales) { //расчет общей суммы продаж
+        long totSales = 0;
 
-        for (int sale : sales) {
+        for (long sale : sales) {
             totSales = totSales + sale;
         }
         //System.out.println("Сумма всех продаж " + totSales);
         return totSales;
     }
 
-    public int AverageAmountSale(int[] sales) {
-        int average = TotalSales(sales) / sales.length;
+    public long averageAmountSale(long[] sales) { //средняя сумма продаж
+        //int average = totalSales(sales) / sales.length;
         //System.out.println("Средняя сумма " + average);
-        return average;
+        return (totalSales(sales) / sales.length);
     }
 
-    public int NumberMaxMonthAverage(int[] sales) {
+    public int numberMaxMonthAverage(long[] sales) { //номер месяца максимума продаж
         int maxMonth = 0;
 
         for (int i = 0; i < sales.length; i++) {
@@ -29,7 +29,8 @@ public class StatsService {
         //System.out.println("Максимум продаж в " + (maxMonth + 1) + " месяце");
         return maxMonth + 1;
     }
-    public int NumberMinMonthAverage (int[] sales) {
+
+    public int numberMinMonthAverage(long[] sales) {  //номер месяца минимума продаж
         int minMonth = 0;
         for (int i = 0; i < sales.length; i++) {
             if (sales[i] <= sales[minMonth]) {
@@ -40,26 +41,28 @@ public class StatsService {
         return minMonth + 1;
     }
 
-    public int QuantityMonthLessAverage (int[] sales) {
-        int quantityMonth = 0;
+    public int countMonthLessAverage(long[] sales) { //число месяцев продаж ниже среднего
+        int countMonth = 0;
+        long averAmountSale = averageAmountSale(sales);
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] < AverageAmountSale(sales)) {
-                quantityMonth = quantityMonth +1;
+            if (sales[i] < averAmountSale) {
+                countMonth = countMonth + 1;
             }
         }
         //System.out.println("Продажи ниже - " + quantityMonth);
-        return quantityMonth;
+        return countMonth;
     }
 
-    public int QuantityMonthOverAverage (int[] sales) {
-        int quantityMonth = 0;
+    public int countMonthOverAverage(long[] sales) {  //число месяцев продаж выше среднего
+        int countMonth = 0;
+        long averAmountSale = averageAmountSale(sales);
         for (int i = 0; i < sales.length; i++) {
-            if (sales[i] > AverageAmountSale(sales)) {
-                quantityMonth = quantityMonth +1;
+            if (sales[i] > averAmountSale) {
+                countMonth = countMonth + 1;
             }
         }
         //System.out.println("Продажи выше - " + quantityMonth);
-        return quantityMonth;
+        return countMonth;
     }
 
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -9,9 +9,9 @@ public class StatsServiceTest {
     public void TotalSalesTest() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expectedTotalSales = 180;
-        int actualTotalSales = service.TotalSales(sales);
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedTotalSales = 180;
+        long actualTotalSales = service.totalSales(sales);
 
         Assertions.assertEquals(expectedTotalSales, actualTotalSales);
     }
@@ -20,9 +20,9 @@ public class StatsServiceTest {
     public void AverageAmountSaleTest() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expectedAverageAmountSale = 15;
-        int actualAverageAmountSale = service.AverageAmountSale(sales);
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expectedAverageAmountSale = 15;
+        long actualAverageAmountSale = service.averageAmountSale(sales);
 
         Assertions.assertEquals(expectedAverageAmountSale, actualAverageAmountSale);
     }
@@ -31,9 +31,9 @@ public class StatsServiceTest {
     public void NumberMaxMonthAverageTest() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedMaxMonth = 8;
-        int actualMaxMonth = service.NumberMaxMonthAverage(sales);
+        int actualMaxMonth = service.numberMaxMonthAverage(sales);
 
         Assertions.assertEquals(expectedMaxMonth, actualMaxMonth);
     }
@@ -42,9 +42,9 @@ public class StatsServiceTest {
     public void NumberMinMonthAverageTest() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedMinMonth = 9;
-        int actualMinMonth = service.NumberMinMonthAverage(sales);
+        int actualMinMonth = service.numberMinMonthAverage(sales);
 
         Assertions.assertEquals(expectedMinMonth, actualMinMonth);
     }
@@ -53,9 +53,9 @@ public class StatsServiceTest {
     public void QuantityMonthLessAverageTest() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedQuantityMonth = 5;
-        int actualQuantityMonth = service.QuantityMonthLessAverage(sales);
+        int actualQuantityMonth = service.countMonthLessAverage(sales);
 
         Assertions.assertEquals(expectedQuantityMonth, actualQuantityMonth);
     }
@@ -64,9 +64,9 @@ public class StatsServiceTest {
     public void QuantityMonthOverAverageTest() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expectedQuantityMonth = 5;
-        int actualQuantityMonth = service.QuantityMonthOverAverage(sales);
+        int actualQuantityMonth = service.countMonthOverAverage(sales);
 
         Assertions.assertEquals(expectedQuantityMonth, actualQuantityMonth);
     }
